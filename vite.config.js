e666036bebaf70aa20c6c21b82dc27eaaea53e2f
Sayed Haider Al Hashemi <sayedhaider2002@gmail.com>
2026-02-10 10:31:04 +0300

    fix: set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/To-do-list/', // Update this to match your repository name
   plugins: [
     react(),
     tailwindcss(),
